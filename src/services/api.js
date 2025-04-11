eb9674cb412d139fd9ee2ca4a57d4b5b8b6c515b
kanak035 <deepsaurabh4362@gmail.com>
2025-04-11 23:42:54 +0530

    xxx

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL, // e.g., https://yttrackbackend.onrender.com
+  baseURL: "https://yttrackbackend.onrender.com/",
   withCredentials: true, // if needed for cookies/auth
 });
 
